8b42d0f810d68ba31618a070e5bfaf1f262de957
Ion Ostafi <ion.ostafi@ellation.com>
2017-09-20 09:13:13 +0300

    Start making the architecure

---
+++ architecture.md
@@ -0,0 +1,19 @@
+presenting layer <-[presentation models]- business logic
+business logic <-[data models]- data base
+business logic <-[input events]- input layer
+
+Models : presentation, data
+Logic: business, presentation, query
+View: console text
+
+
+entry point : main();
+
+Data/Events flow:
+synchronous input -> console input layer -[actions]-> business layer
+business layer -[requests]-> data layer
+
+##Layers responsabilities
+
+### Input layer
+- receive input from user
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,47 @@
-use std::env;
-use std::fs::File;
-use std::io::prelude::*;
+extern crate clap;
+use clap::{Arg, App};
+extern crate csv;
+extern crate rustc_serialize;
+
+#[derive(RustcDecodable)]
+#[derive(Debug)]
+struct PasswordRecord {
+    url: String,
+    username: String,
+    password: String,
+    extra: String,
+    name: String,
+    grouping: String,
+    fav: u32,
+    custom_fields: String,
+    last_modified_time: i64,
+    uid: i64,
+    username_label: String,
+    password_label: String,
+    website_label: String,
+    notes_label: String,
+    password_set_date: u64,
+    flags: i32,
+    image_index: i32,
+    data_version: i32
+}
 
 fn main() {
-    println!("Hello, world!");
+    let passwords = csv::Reader::from_file("/Users/iostafi/Documents/Documents_offline/pk_backup_2016-09-30.txt");
+    if let Ok(mut ps) = passwords {
+        for p in ps.decode() {
+            if let Ok(pass) = p {
+                let record: PasswordRecord = pass;
+                println!("{:?}", record);
+            }
+        }
+    }
+}
+
+fn parse_input_arguments() {
+    let matches = App::new("zfiles")
+        .version("0.1.0")
+        .author("Ion Ostafi <ostafi_ion@yahoo.com>")
+        .about("tool for saving passwords")
+        .get_matches();
 }
