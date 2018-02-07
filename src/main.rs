2570239f0a72d82881b165cc9373228e1652ae61
Ion Ostafi <ion.ostafi@ellation.com>
2018-02-07 16:54:45 +0200

    Add subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 extern crate clap;
-use clap::{Arg, App};
+use clap::{Arg, App, SubCommand};
 extern crate csv;
 extern crate rustc_serialize;
 mod password_printer;
@@ -7,11 +7,19 @@ mod password_records;
 use password_records::PasswordRecord;
 
 fn main() {
-    let records = read_records();
-    password_printer::print_password_records(records);
+    let app = create_app();
+    let matches = app.get_matches();
+    match matches.subcommand() {
+        ("show", Some(show_cmd)) => {
+            if show_cmd.is_present("--names") {
+                let records = read_records();
+                password_printer::print_password_records_titles(records);
+            }
+        },
+        _ => {},
+    }
 }
 
-#[allow(dead_code)]
 fn read_records() -> Vec<PasswordRecord> {
     let mut password_records: Vec<PasswordRecord> = Vec::new();
     let csv_values = csv::Reader::from_file("/Users/iostafi/Documents/Documents_offline/pk_backup_2016-09-30.txt");
@@ -26,11 +34,16 @@ fn read_records() -> Vec<PasswordRecord> {
     password_records
 }
 
-#[allow(dead_code)]
-fn parse_input_arguments() {
-    let matches = App::new("zfiles")
+fn create_app<'a>() -> App<'a, 'a> {
+    App::new("zfiles")
         .version("0.1.0")
         .author("Ion Ostafi <ostafi_ion@yahoo.com>")
         .about("tool for saving passwords")
-        .get_matches();
+        .subcommand(
+            SubCommand::with_name("show")
+            .about("print passwords")
+            .subcommand(
+                SubCommand::with_name("--names")
+            )
+        )
 }
--- src/password_printer.rs
+++ src/password_printer.rs
@@ -2,22 +2,12 @@ extern crate console;
 use self::console::Term;
 use password_records::PasswordRecord;
 
-pub fn print_password_titles(titles: Vec<String>) {
-    let term = Term::stdout();
-    for title in titles {
-        let success  = term.write_line(title.as_ref());
-        if success.is_err() {
-            println!("Error outputing titles!");
-        }
-    }
-}
-
-pub fn print_password_records(records: Vec<PasswordRecord>) {
+pub fn print_password_records_titles(records: Vec<PasswordRecord>) {
     let term = Term::stdout();
     for record in records {
-        let success  = term.write_line(record.name.as_ref());
-        if success.is_err() {
-            println!("Error outputing titles!");
+        let result = term.write_line(record.name.as_ref());
+        if result.is_err() {
+            println!("Cannot output titles!");
         }
     }
 }
